14b3299a4a023d0f2e4b6de79c30fee6075a202a
doapp-ryanp <ryanp@doapps.com>
2016-10-27 17:58:58 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# serverless-browserify
-Speed up your node based lambda's
+# Serverless Browserify Plugin
+
+[![serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+
+A [Serverless](https://serverless.com) v1.0 plugin that uses [Browserify](https://github.com/substack/node-browserify) to bundle your NodeJS Lambda functions.
+
+Lambda's with less code start faster and run faster.  Lambda also has an account wide [deployment package size limit](http://docs.aws.amazon.com/lambda/latest/dg/limits.html).  Furthermore, [aws-sdk-js](https://github.com/aws/aws-sdk-js) now officially [supports browserify](https://github.com/aws/aws-sdk-js/issues/696).  I prefer Browserify over [webpack](https://webpack.github.io/) because I have found it supports more modules, optimizes better, and requires less configuration.   
+
+## Install
+
+From your serverless project run:
+```
+npm install serverless-plugin-browserify --save-dev
+```
+
+Add the plugin to your `serverless.yml` file and set `package.individually` to `true`:
+
+```yaml
+plugins:
+  - serverless-plugin-browserify
+package:
+  individually: true
+```
+
+`package.individually` is required because it makes configuration more straight forward, and if you are not packaging individually size is not a concern of yours in the 1st place.
+
+## Configure
+
+The base config for browserify is read from the `custom.browserify` section of `serverless.yml`.  Most [browserify options](https://github.com/substack/node-browserify#browserifyfiles--opts) are supported but only a few are needed for common use cases, most are handled for you.  This plugin adds one special option `disable` which if `true` will bypass this plugin.
+
+The base config can be over-ridden on a function by function basis.  Again `custom.browserify` is not required and should not even need to be defined in most cases.
+
+```yaml
+custom:
+  browserify:
+  
+```
+
+## Usage
+
+## FAQ
+
+- **Why is UglifyJS not built-in?** No ES6 support.  [Issue](https://github.com/mishoo/UglifyJS2/issues/448) been open since 2014.
+- **My code is not bundling correctly** The bundled code is always stored in a tmp dir on your computer.  Set `SLS_DEBUG=true` then re-run your command to output the directory.  Fish Shell ex: `env SLS_DEBUG=true sls browserify`  
+
+## Thanks
+
+To [serverless-webpack](https://github.com/elastic-coders/serverless-webpack) for a serverless plugin implementation reference 
---
+++ index.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const BbPromise = require('bluebird');
+
+const validate  = require('./lib/validate'),
+      configure = require('./lib/configure'),
+      // const compile = require('./lib/compile');
+      cleanup   = require('./lib/cleanup');
+// const run = require('./lib/run');
+// const serve = require('./lib/serve');
+// const packExternalModules = require('./lib/packExternalModules');
+
+class SlsBrowserify {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options    = options;
+
+    Object.assign(
+      this,
+      validate,
+      configure
+      // compile,
+      // cleanup,
+      // run,
+      // serve,
+      // packExternalModules
+    );
+
+    this.commands = {
+      browserify: {
+        usage:           'Bundle NodeJS lambda with Browserify',
+        lifecycleEvents: [
+          'validate',
+          'compile',
+        ],
+        options:         {
+          out: {
+            usage:    'Path to output directory',
+            shortcut: 'o',
+          },
+        },
+        commands:        {
+          // invoke: {
+          //   usage:           'Run a function locally from the webpack output bundle',
+          //   lifecycleEvents: [
+          //     'invoke',
+          //   ],
+          //   options:         {
+          //     function: {
+          //       usage:    'Name of the function',
+          //       shortcut: 'f',
+          //       required: true,
+          //     },
+          //     path:     {
+          //       usage:    'Path to JSON file holding input data',
+          //       shortcut: 'p',
+          //     },
+          //   },
+          // },
+          // watch:  {
+          //   usage:           'Run a function from the webpack output bundle every time the source is changed',
+          //   lifecycleEvents: [
+          //     'watch',
+          //   ],
+          //   options:         {
+          //     function: {
+          //       usage:    'Name of the function',
+          //       shortcut: 'f',
+          //       required: true,
+          //     },
+          //     path:     {
+          //       usage:    'Path to JSON file holding input data',
+          //       shortcut: 'p',
+          //     },
+          //   },
+          // },
+          // serve:  {
+          //   usage:           'Simulate the API Gateway and serves lambdas locally',
+          //   lifecycleEvents: [
+          //     'serve',
+          //   ],
+          //   options:         {
+          //     port: {
+          //       usage:    'The local server port',
+          //       shortcut: 'p',
+          //     },
+          //   },
+          // },
+        },
+      },
+    };
+
+    this.hooks = {
+      'before:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
+        .then(this.validate),
+      // .then(this.compile)
+      // .then(this.packExternalModules),
+
+      'after:deploy:createDeploymentArtifacts': () => BbPromise.bind(this)
+        .then(this.cleanup),
+
+      'browserify:validate': () => BbPromise.bind(this)
+        .then(this.validate)
+        .then(this.configure)
+        .catch((e)=> {
+          if ('skip' != e.statusCode) {
+            throw e;
+          } else {
+            console.error(e.message, 'SKIPPING bundling');
+          }
+        }),
+      //
+      // 'webpack:compile': () => BbPromise.bind(this)
+      //   .then(this.compile)
+      //   .then(this.packExternalModules),
+      //
+      // 'webpack:invoke:invoke': () => BbPromise.bind(this)
+      //   .then(this.validate)
+      //   .then(this.compile)
+      //   .then(this.run)
+      //   .then(out => this.serverless.cli.consoleLog(out)),
+      //
+      // 'webpack:watch:watch': () => BbPromise.bind(this)
+      //   .then(this.validate)
+      //   .then(this.watch),
+      //
+      // 'webpack:serve:serve': () => BbPromise.bind(this)
+      //   .then(this.validate)
+      //   .then(this.serve),
+    };
+  }
+}
+
+module.exports = SlsBrowserify;
